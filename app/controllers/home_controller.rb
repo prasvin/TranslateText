00b0ba02d7460c958edc0732e31a6831cd2e2dd6
Dipil <dipil.saud@sprout-technology.com>
2011-04-12 17:40:43 +0545

    Home Views

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,8 +4,14 @@ class HomeController < ApplicationController
 
   def user
     if current_user.type == "Requester"
-
+      render 'requester'
     end
+  end
+
+  def requester
+  end
+
+  def translator
   end
 
 end
